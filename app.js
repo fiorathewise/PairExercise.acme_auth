fa631199dd65fbb1c92e8894cf84a280fdfa8a5c
fiorathewise <fiorawise@ATHENA.localdomain>
2021-05-24 18:03:58 -0400

    totally broken code?

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const app = express();
 app.use(express.json());
-const { models: { User }} = require('./db');
-const path = require('path');
+const {
+   models: { User },
+} = require("./db");
+const path = require("path");
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.post('/api/auth', async(req, res, next)=> {
-  try {
-    res.send({ token: await User.authenticate(req.body)});
-  }
-  catch(ex){
-    next(ex);
-  }
+app.post("/api/auth", async (req, res, next) => {
+   try {
+      const user = await User.authenticate(req.body);
+		if (!user) res.sendStatus(404);
+		res.send(user);
+   } catch (ex) {
+      next(ex);
+   }
 });
 
-app.get('/api/auth', async(req, res, next)=> {
-  try {
-    res.send(await User.byToken(req.headers.authorization));
-  }
-  catch(ex){
-    next(ex);
-  }
+app.get("/api/auth", async (req, res, next) => {
+   try {
+      const token = req.headers.authorization;
+		const data = await jwt.verify(token, "testKey");
+		console.log(data, 'data');
+		const user = await User.findByPk(data.userId);
+		res.send(user);
+   } catch (ex) {
+      next(ex);
+   }
 });
 
-app.use((err, req, res, next)=> {
-  console.log(err);
-  res.status(err.status || 500).send({ error: err.message });
+app.use((err, req, res, next) => {
+   console.log(err);
+   res.status(err.status || 500).send({ error: err.message });
 });
 
 module.exports = app;
